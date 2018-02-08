43909bb4e7fa8b2cb2b3dc4c9b91b9bf60d64436
mahesh.thuma <mahesh.thuma@geniussports.com>
2018-02-08 13:31:50 +0000

    commit

--- src/test/scala/opap/HttpPlaceBet.scala
+++ src/test/scala/opap/HttpPlaceBet.scala
@@ -8,54 +8,60 @@ import net.liftweb.json._
 
 
 class HttpPlaceBet extends Simulation{
-
   val sentHeaders = Map("Content-Type" -> "application/json",
-                        "origin" -> "https://app.geniusbet-7442.uat.aws.betgenius.com",
-                        "pragma" -> "no-cache",
-                        "referer" -> "https://app.geniusbet-7442.uat.aws.betgenius.com/web/xdomain/proxy.html",
-                        "user-agent" -> "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36")
+    "origin" -> "https://app.geniusbet-7442.uat.aws.betgenius.com",
+    "pragma" -> "no-cache",
+    "referer" -> "https://app.geniusbet-7442.uat.aws.betgenius.com/web/xdomain/proxy.html",
+    "user-agent" -> "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36")
+  var request: String = null
 
-  val jsonBody : String = "{'id':'8ae0a3df-6a81-4cdf-93ec-b632a4a17673','clientId':'1-10','channelId':0,'ipAddress':'1.1.1.1','requestGuid':'REQ-ba2f45b2-79fd-4c44-a29d-f9d6f71097bd','singleBets':[{'leg':{'legId':1,'selection':{'fixedMarketId':'227044','eventId':'60586','outcomeId':'2','marketTypeCode':'WIN','dividendTypeCode':'FIXED','prices':[{'value':'1.92000000','priceType':'WIN'}],'points':0}},'totalStake':0.5,'interceptId':0,'walletAccountId':'AT_MAIN-0','betReferenceGuid':'63e3a65c-f3b6-4ce4-b271-c0c2c512e4e5','bonus':{'freebetId':0,'walletBonusId':0,'isBonusBet':false}}],'multiBets':[]}"
+  getMarketData()
 
-  val scn = scenario("OpapHitsPerSecond")
-            .exec(http("Place Bet")
-              .post("https://app.geniusbet-7442.uat.aws.betgenius.com/tbs.betting.api/api/v1/sportsbook/clients/1-925967590/bets")
-              .headers(sentHeaders)
-              .body(StringBody(jsonBody)).asJSON
-              .check(status.is(200))
-            )
-/*
+  val scn = scenario("Place Bets")
+    .exec(http("Place Bet")
+      .post("https://app.geniusbet-7442.uat.aws.betgenius.com/tbs.betting.api/api/v1/sportsbook/clients/1-925967590/bets")
+      .headers(sentHeaders)
+      .body(StringBody(request)).asJSON
+      .check(status is(200))
+      .check(regex("ticketNumber").exists)
+    )
 
-  def getMarketData(){
+  setUp(
+    scn.inject(atOnceUsers(50))
+  )
+
+
+  def getMarketData() {
     var uri: URI = URI.create("https://opap-uat.betstream.betgenius.com/betstream-view/getMarkets/source/7442.json")
     var tokener = scala.io.Source.fromURL(uri.toURL()).mkString
-    val mailAccountString ="""
-val json = parse(mailAccountString)
+    val json = parse(tokener)
 
-    for(data <- json){tokener
-      //println("BetgeniusFixtureId's ---------------->"+data.\("betgeniusFixtureId").values)
-      //val list = List[String] = data.\("betgeniusFixtureId").values
-      //println("Size --------->"+Seq(data.\("betgeniusFixtureId").values))
-      val m = data.extract[MarketSelections]
-      println("betgeniusFixtureId ------->"+m.betgeniusFixtureId)
+    val marketTypeCode = (json \ "marketType").get
+
+    for (i <- 0 to marketTypeCode.toString.size) {
+      if (marketTypeCode.apply(i) == (JInt(322))) {
+        val JInt(betgeniusId) = (json \ "betgeniusFixtureId").get.apply(i)
+        val JInt(marketType) = (json \ "marketType").get.apply(i)
+        val JString(marketIdCode) = (json \ "sourceMarketId").get.apply(i)
+        val JString(sourceSelectioId) = (json \ "marketSelections").get.apply(i).\("sourceSelectionId").apply(1)
+        val JDouble(stake) = (json \ "marketSelections").get.apply(i).\("price").\("decimalPrice").apply(1)
+        val JString(sport) = (json \ "sport").get.apply(i)
+
+        println("betGeniusFixtureID ------------->" + betgeniusId)
+        println("Market Type found  -------------> " + marketType)
+        println("Market ID  -------------> " + marketIdCode)
+        println("source selection id ------------> " + sourceSelectioId.split("-").apply(1))
+        println("stake -------------------------->" + stake)
+        println("sport -------------------------->" + sport)
+
+        request = s"{'id':'8ae0a3df-6a81-4cdf-93ec-b632a4a17673','clientId':'1-10','channelId':0,'ipAddress':'1.1.1.1','requestGuid':'REQ-ba2f45b2-79fd-4c44-a29d-f9d6f71097bd','singleBets':[{'leg':{'legId':1,'selection':{'fixedMarketId':'${sourceSelectioId.split("-").apply(1)}','eventId':'$marketIdCode','outcomeId':'1','marketTypeCode':'WIN','dividendTypeCode':'FIXED','prices':[{'value':'$stake','priceType':'WIN'}],'points':0}},'totalStake':0.25,'interceptId':0,'walletAccountId':'AT_MAIN-0','betReferenceGuid':'63e3a65c-f3b6-4ce4-b271-c0c2c512e4e5','bonus':{'freebetId':0,'walletBonusId':0,'isBonusBet':false}}],'multiBets':[]}"
+
+        println("request Body " + request)
+        return
+      }
     }
 
 
-    /*val elements = (json \\ "marketSelections").children
-    for ( acct <- elements ) {
-      val m = acct.\("sourceSelectionId")
-      //extract[MarketSelections]
-      println("sourceSelectionId "+m)
-
-    }*/
-
   }
-
-  getMarketData()
-*/
-
-  setUp(
-    scn.inject(atOnceUsers(1))
-  )
 
 }
--- src/test/scala/opap/ReadJson.scala
+++ src/test/scala/opap/ReadJson.scala
@@ -3,25 +3,72 @@ package opap
 import java.net.URI
 
 import io.gatling.core.Predef._
-import net.liftweb.json.{JsonAST, parse}
+import io.gatling.http.Predef._
+import net.liftweb.json._
 
-import scala.util.parsing.json.JSONArray
 
-class ReadJson extends Simulation{
+class ReadJson extends Simulation {
 
-  def getMarketData(){
+  val sentHeaders = Map("Content-Type" -> "application/json",
+    "origin" -> "https://app.geniusbet-7442.uat.aws.betgenius.com",
+    "pragma" -> "no-cache",
+    "referer" -> "https://app.geniusbet-7442.uat.aws.betgenius.com/web/xdomain/proxy.html",
+    "user-agent" -> "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36")
+
+  //val jsonBody: String = "{'id':'8ae0a3df-6a81-4cdf-93ec-b632a4a17673','clientId':'1-10','channelId':0,'ipAddress':'1.1.1.1','requestGuid':'REQ-ba2f45b2-79fd-4c44-a29d-f9d6f71097bd','singleBets':[{'leg':{'legId':1,'selection':{'fixedMarketId':'227044','eventId':'60586','outcomeId':'2','marketTypeCode':'WIN','dividendTypeCode':'FIXED','prices':[{'value':'1.92000000','priceType':'WIN'}],'points':0}},'totalStake':0.5,'interceptId':0,'walletAccountId':'AT_MAIN-0','betReferenceGuid':'63e3a65c-f3b6-4ce4-b271-c0c2c512e4e5','bonus':{'freebetId':0,'walletBonusId':0,'isBonusBet':false}}],'multiBets':[]}"
+  var request: String = null
+  var mt: String = null
+
+  def getMarketData() {
     var uri: URI = URI.create("https://opap-uat.betstream.betgenius.com/betstream-view/getMarkets/source/7442.json")
     var tokener = scala.io.Source.fromURL(uri.toURL()).mkString
     val json = parse(tokener)
 
-   // println("Market Type "+(json.\("marketType")))
-    val marketypes: JsonAST.JValue = (json.\("marketType"))
-    for(mt <- (json.\("marketType"))){
-      println("Values ---------->"+ Seq(marketypes))
+    val marketTypeCode = (json \ "marketType").get
+    val sourceSelectionId = (json \ "marketSelections").get
+    val bgFixtureId = (json \ "betgeniusFixtureId").get
+    val sportName = (json \ "sport").get
+    val marketId = (json \ "sourceMarketId").get
+
+    /*mt = markettype.toString
+    println("betgeniusFixtureId count------->" + mt.size + " First element--->" + markettype.apply(0).toString)
+    */
+    for (i <- 0 to marketTypeCode.toString.size) {
+      if (marketTypeCode.apply(i) == (JInt(322))) {
+        val JInt(betgeniusId) = bgFixtureId.apply(i)
+        val JInt(marketType) = marketTypeCode.apply(i)
+        val JString(marketIdCode) = marketId.apply(i)
+        val JString(sourceSelectioId) = sourceSelectionId.apply(i).\("sourceSelectionId").apply(1)
+        val JDouble(stake) = sourceSelectionId.apply(i).\("price").\("decimalPrice").apply(1)
+        val JString(sport) = sportName.apply(i)
+        println("betGeniusFixtureID ------------->" + betgeniusId)
+        println("Market Type found  -------------> " + marketType)
+        println("Market ID  -------------> " + marketIdCode)
+        println("source selection id ------------> " + sourceSelectioId.split("-").apply(1))
+        println("stake -------------------------->" + stake)
+        println("sport -------------------------->" + sport)
+        request = s"{'id':'8ae0a3df-6a81-4cdf-93ec-b632a4a17673','clientId':'1-10','channelId':0,'ipAddress':'1.1.1.1','requestGuid':'REQ-ba2f45b2-79fd-4c44-a29d-f9d6f71097bd','singleBets':[{'leg':{'legId':1,'selection':{'fixedMarketId':'${sourceSelectioId.split("-").apply(1)}','eventId':'$marketIdCode','outcomeId':'1','marketTypeCode':'WIN','dividendTypeCode':'FIXED','prices':[{'value':'$stake','priceType':'WIN'}],'points':0}},'totalStake':0.25,'interceptId':0,'walletAccountId':'AT_MAIN-0','betReferenceGuid':'63e3a65c-f3b6-4ce4-b271-c0c2c512e4e5','bonus':{'freebetId':0,'walletBonusId':0,'isBonusBet':false}}],'multiBets':[]}"
+        println("request Body " + request)
+        return
+      }
     }
+
 
   }
 
   getMarketData()
+
+  val scn = scenario("Place Bets")
+    .exec(http("Place Bet")
+      .post("https://app.geniusbet-7442.uat.aws.betgenius.com/tbs.betting.api/api/v1/sportsbook/clients/1-925967590/bets")
+      .headers(sentHeaders)
+      .body(StringBody(request)).asJSON
+      .check(status is(200))
+      .check(regex("ticketNumber").exists)
+    )
+
+  setUp(
+    scn.inject(atOnceUsers(25))
+  )
 
 }
